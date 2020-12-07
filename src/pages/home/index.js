c876c586b13fc4594edaad3008fb255ce2745934
WilliamChang80 <william_chang80@rocketmail.com>
2020-12-07 17:48:50 +0700

    Add styling on register and homepage

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,6 +3,8 @@ import Layout from "../../components/layout"
 import { getUser } from "../../services/auth"
 import { Link } from "gatsby"
 import { navigate } from "../../../.cache/gatsby-browser-entry"
+import AgencyImage from "../../images/agency.jpg"
+import ClientImage from "../../images/client.jpg"
 
 const ChooseUserTypePage = () => {
   const { data } = getUser()
@@ -14,17 +16,32 @@ const ChooseUserTypePage = () => {
     <Layout>
       <div
         className={"d-flex justify-content-around align-items-center"}
+        style={{ fontSize: "4rem", color: "#5a10c9" }}
+      >
+        <b>Who Are You?</b>
+      </div>
+      <div
+        className={"d-flex justify-content-around align-items-center"}
         style={{ height: "70vh" }}
       >
         <Link
+          className="link"
           to={
             data.user.roles.indexOf("CLIENT") !== -1
               ? "/home/type/client"
               : "/register/user/client"
           }
           className={"shadow-lg p-5 text-dark"}
+          style={{ borderRadius: "2rem" }}
         >
-          <h1 className={"text-center"}>Customer</h1>
+          <img
+            className="home-image"
+            style={{ width: "20vw", height: "40vh" }}
+            src={ClientImage}
+          ></img>
+          <h1 className={"text-center"} style={{ color: "#5a10c9" }}>
+            Customer
+          </h1>
           <p>
             {data.user.roles.indexOf("CLIENT") !== -1
               ? "Checkout your user home"
@@ -32,14 +49,23 @@ const ChooseUserTypePage = () => {
           </p>
         </Link>
         <Link
+          className="link"
           to={
             data.user.roles.indexOf("AGENCIES") !== -1
               ? "/home/type/agency"
               : "/register/user/agency"
           }
           className={"shadow-lg p-5 text-dark"}
+          style={{ borderRadius: "2rem" }}
         >
-          <h1 className={"text-center"}>Agency</h1>
+          <img
+            className="home-image"
+            style={{ width: "30vw", height: "40vh" }}
+            src={AgencyImage}
+          ></img>
+          <h1 className={"text-center"} style={{ color: "#5a10c9" }}>
+            Agency
+          </h1>
           <p>
             {data.user.roles.indexOf("AGENCIES") !== -1
               ? "Checkout your agency home"
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -4,72 +4,95 @@ import { navigate } from "../../../.cache/gatsby-browser-entry"
 import Layout from "../../components/layout"
 
 const RegisterPage = () => {
-  const [state,setState] = useState({
-    email : '',
-    name : '',
-    password : '',
-    confirmPassword: '',
-    errorMessage: '',
+  const [state, setState] = useState({
+    email: "",
+    name: "",
+    password: "",
+    confirmPassword: "",
+    errorMessage: "",
   })
 
-  const handleChange = (e) => {
-    const {id, value} = e.target;
+  const handleChange = e => {
+    const { id, value } = e.target
     setState(prevState => ({
       ...prevState,
-      [id] : value
+      [id]: value,
     }))
   }
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (confirmPasswordCheck()){
-      const cred = {email : state.email, password : state.password}
-      const promise = await register(cred);
-      const {message,code} = promise;
-      if(code === 200){
-        navigate('/login');
-      }else{
-        setState(prevState => ({ ...prevState,errorMessage : message}))
+  const handleSubmit = async e => {
+    e.preventDefault()
+    if (confirmPasswordCheck()) {
+      const cred = { email: state.email, password: state.password }
+      const promise = await register(cred)
+      const { message, code } = promise
+      if (code === 200) {
+        navigate("/login")
+      } else {
+        setState(prevState => ({ ...prevState, errorMessage: message }))
       }
-    }
-    else{
-      setState(prevState => ({...prevState,errorMessage: "Password Not Match"}))
+    } else {
+      setState(prevState => ({
+        ...prevState,
+        errorMessage: "Password Not Match",
+      }))
     }
   }
 
   const confirmPasswordCheck = () => {
-    if (state.confirmPassword === state.password) return true;
-    else return false;
+    if (state.confirmPassword === state.password) return true
+    else return false
   }
-  if (isLoggedIn()){
-    navigate('user/index')
+  if (isLoggedIn()) {
+    navigate("user/index")
   }
-  return(
-    <Layout>
-      <div className={'mt-3'}>
+  return (
+    <Layout className="register-page">
+      <div className={"mt-3"}>
         <h1>Hi there!</h1>
         <p>New here? Let's register first :)</p>
       </div>
 
-      {(state.errorMessage!=='')&&<div className='alert alert-danger'>{state.errorMessage}</div>}
+      {state.errorMessage !== "" && (
+        <div className="alert alert-danger">{state.errorMessage}</div>
+      )}
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="exampleInputEmail1">Email address</label>
-          <input type="text" className="form-control" id="email" aria-describedby="emailHelp"
-                 placeholder="Enter email" onChange={handleChange}/>
-          <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone
-            else.</small>
+          <input
+            type="text"
+            className="form-control"
+            id="email"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+            onChange={handleChange}
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
         </div>
         <div className="form-group">
           <label htmlFor="exampleInputPassword1">Password</label>
-          <input type="password" className="form-control" id="password" onChange={handleChange} placeholder="Password"/>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            onChange={handleChange}
+            placeholder="Password"
+          />
         </div>
         <div className="form-group">
           <label htmlFor="exampleInputPassword1">Confirm Password</label>
-          <input type="password" className="form-control" id="confirmPassword" onChange={handleChange} placeholder="Confirm Password"/>
+          <input
+            type="password"
+            className="form-control"
+            id="confirmPassword"
+            onChange={handleChange}
+            placeholder="Confirm Password"
+          />
         </div>
 
-        <input type="submit" className="btn btn-primary"/>
+        <input type="submit" className="btn btn-primary" />
       </form>
     </Layout>
   )
