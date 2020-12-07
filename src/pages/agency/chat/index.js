ac42e318748d144316d621c1fe49b60786ee09e1
WilliamChang80 <william_chang80@rocketmail.com>
2020-12-07 22:12:52 +0700

    Fix

--- src/pages/agency/chat/index.js
+++ src/pages/agency/chat/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 import AgencyChatPage from "../../../components/pages/agencyPages/agencyChatPage"
 
-const ChatPage = ({location}) => {
-  return <AgencyChatPage choosenAgencyId={location.state.thisAgencyId} />
+const ChatPage = ({ location }) => {
+  return <AgencyChatPage choosenAgencyId={location?.state?.thisAgencyId} />
 }
 
 export default ChatPage
